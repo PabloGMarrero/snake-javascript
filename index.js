b39fc05c1680c6525b3a962f9cf9160e31783049
PabloGMarrero <pablo.g.marrero@gmail.com>
2019-12-26 14:09:10 -0300

    Added basic template html and inject into server

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3600
 
-app.get('/', (res, req) => req.send("Hello world!"))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 
 app.listen(port, () => console.log(`Example listening on port ${port}`))
