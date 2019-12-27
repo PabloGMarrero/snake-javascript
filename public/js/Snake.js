d16e918313d77d8368c1078aeed64b830da99d72
PabloGMarrero <pablo.g.marrero@gmail.com>
2019-12-26 21:01:20 -0300

    first way to snake die

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -2,12 +2,14 @@ export default class Snake {
     constructor(scene){
         this.scene = scene;
         this.lastMoveTime = 0;
-        this.moveInteval = 500;
+        this.moveInteval = 400;
         this.tileSize = 20 ;
         this.direction = Phaser.Math.Vector2.RIGHT;
         this.body = [];
+        
         this.body.push(
-            this.scene.add.rectangle(100, 100, this.tileSize, this.tileSize, 0x00ff00).setOrigin(0)
+            this.scene.add.rectangle(this.scene.game.config.width / 2, this.scene.game.config.height / 2,
+                 this.tileSize, this.tileSize, 0x00ff00).setOrigin(0)
         );
 
         this.apple = this.scene.add.rectangle(0, 0, this.tileSize, this.tileSize, 0xff00000).setOrigin(0)
@@ -25,13 +27,29 @@ export default class Snake {
     }
 
     move(){
+
+        let xPosition = this.body[0].x + this.direction.x * this.tileSize;
+        let yPosition = this.body[0].y + this.direction.y * this.tileSize;
+
+        let appleX = this.apple.x;
+        let appleY = this.apple.y
+        
+        if (appleX === xPosition && appleY ===yPosition) {
+            this.eatenTheApple()
+        }
+
         for(let index = this.body.length - 1; index> 0; index-- ){
             this.body[index].x = this.body[index-1].x;
             this.body[index].y = this.body[index-1].y;
         }
 
-        this.body[0].x += this.direction.x *20 ;
-        this.body[0].y += this.direction.y *20 ;
+        this.body[0].x = xPosition;
+        this.body[0].y = yPosition;
+
+        if (this.body[0].x < 0 || this.body[0].x >=this.scene.game.config.width || 
+            this.body[0].y < 0 || this.body[0].y >=this.scene.game.config.height ){
+                this.scene.scene.restart();
+            }
 
     }
 
@@ -40,8 +58,14 @@ export default class Snake {
         this.apple.y = Math.floor(Math.random() * this.scene.game.config.width  / this.tileSize) * this.tileSize;
     }
 
+    eatenTheApple(){
+        this.body.push(
+            this.scene.add.rectangle(0, 0, this.tileSize, this.tileSize, 0x00ff00).setOrigin(0)
+        );
+        this.randomPositionApple();
+        this.moveInteval -= 10;
+    }
     keydown(event){
-        console.log(event)
         switch (event.keyCode){
             case 37://left
                 this.direction = Phaser.Math.Vector2.LEFT;
